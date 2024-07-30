71caa38c063a167ce8aeda9a6e2f2c7f91c5d87a
michalokulski <michal.okulski@gmail.com>
2024-07-30 09:00:53 +0200

    Update warning.ps1

--- warning.ps1
+++ warning.ps1
@@ -1,17 +1,18 @@
-Start-Process powershell -ArgumentList @'
-    # Display warning message
-    Write-Host "`n⚠️ WARNING ⚠️"
-    Write-Host "If you proceed with the provided input, ALL content on your disk drive will be PERMANENTLY DELETED. This includes but is not limited to:"
-    Write-Host "- All installed applications"
-    Write-Host "- All personal and system files"
-    Write-Host "- All system configurations"
-    Write-Host "Data that is deleted in this manner CANNOT BE RECOVERED. If you have any files or data that you wish to keep, please ensure that you have created a secure backup."
-    Write-Host "Please consider the consequences carefully before proceeding. Once the data is deleted, it will be gone forever. Proceed with extreme caution.`n"
+# Create a new PowerShell process
+#Start-Process powershell -ArgumentList @'
+    # Display warning message in red
+    Write-Host "`n⚠️ WARNING ⚠️" -ForegroundColor Red
+    Write-Host "If you proceed with the provided input, ALL content on your disk drive will be PERMANENTLY DELETED. This includes but is not limited to:" -ForegroundColor Red
+    Write-Host "- All installed applications" -ForegroundColor Red
+    Write-Host "- All personal and system files" -ForegroundColor Red
+    Write-Host "- All system configurations" -ForegroundColor Red
+    Write-Host "Data that is deleted in this manner CANNOT BE RECOVERED. If you have any files or data that you wish to keep, please ensure that you have created a secure backup." -ForegroundColor Red
+    Write-Host "Please consider the consequences carefully before proceeding. Once the data is deleted, it will be gone forever. Proceed with extreme caution.`n" -ForegroundColor Red
 
     # Loop until the user enters "UNDERSTAND"
     while ($true) {
         # Prompt the user for input
-        $input = Read-Host "Enter a command (type UNDERSTAND if you are sure!)"
+        $input = Read-Host "Enter a command (type UNDERSTAND to exit)"
 
         # If the user entered "UNDERSTAND", exit the script
         if ($input -eq "UNDERSTAND") {
@@ -19,7 +20,7 @@ Start-Process powershell -ArgumentList @'
         }
         else {
             # If the user entered anything other than "UNDERSTAND", restart the computer
-            #Restart-Computer -Force
+            Restart-Computer -Force
         }
     }
-'@
+#'@
